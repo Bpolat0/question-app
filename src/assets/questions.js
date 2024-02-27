0b1fd2e74acb6390667830e9aff641e4b555f66e
Mehmet Batuhan Polat <105828326+Bpolat0@users.noreply.github.com>
2024-02-27 22:32:38 +0300

    Add images to questions

--- src/assets/questions.js
+++ src/assets/questions.js
@@ -1,23 +1,32 @@
+import cinSeddi from './pictures/cin-seddi.jpg';
+import pamuk from './pictures/pamuk.jpg';
+import tarkan from './pictures/tarkan.jpg';
+import fobi from './pictures/fobi.jpg';
+import balik from './pictures/balik.jpg';
+import bandira from './pictures/bandira.jpg';
+import billboard from './pictures/billboard.jpg';
+import parfum from './pictures/parfum.jpg';
+import trex from './pictures/trex.jpg';
 
 const exams = [
   {
     question: "Çin Seddini oluşturan taşlar birbirine ne ile tutturulmuştur?",
     options: ["Bambu Harcı", "Anne Duası", "Pirinç Unu", "Noodle"],
     answer: "Pirinç Unu",
-    media: "assets/pictures/cin-seddi.jpg",
+    media: cinSeddi,
   },
   {
     question: "İlk Pamuk şekeri bulan kişinin mesleği nedir?",
     options: ["Gıda Mühendisi", "Diş Doktoru", "Ev Hanımı", "Güzellik Uzmanı"],
     answer: "Diş Doktoru",
-    media: "assets/pictures/pamuk.jpg",
+    media: pamuk,
   },
   {
     question:
       "Tarkan'ın 'Hüp' klibini izledikten sonra gaza gelip 'Tarkan keşke beni hüpletseydi' diye açıklamda bulunan kişi kimdir?",
     options: ["Gülben Ergen", "Hülya Avşar", "Harika Avcı", "Sevtap Parman"],
     answer: "Gülben Ergen",
-    media: "assets/pictures/tarkan.jpg",
+    media: tarkan,
   },
   {
     question: "Pteronofobi nedir?",
@@ -28,47 +37,47 @@ const exams = [
       "Kuş tüyüyle gıdıklanma korkusu",
     ],
     answer: "Kuş tüyüyle gıdıklanma korkusu",
-    media: "assets/pictures/fobi.jpg",
+    media: fobi,
   },
   {
     question:
       "Ortalama ömürleri 5 yıl olan Japon balıklarının en uzun yaşayanı Tish, bütün istatistikleri alt üst ederek kaç yıl boyunca hayata tutunmayı başarmıştır?",
     options: ["43", "78", "23", "99"],
     answer: "43",
-    media: "assets/pictures/balik.jpg",
+    media: balik,
   },
   {
     question:
       "90'lara damgasını vuran 'Bandıra Bandıra' şarkısının söz yazarı kimdir?",
     options: ["Sezen Aksu", "Sibel Can", "Mustafa Sandal", "Bülent Ersoy"],
     answer: "Mustafa Sandal",
-    media: "assets/pictures/bandira.jpg",
+    media: bandira,
   },
   {
     question:
       "Hangi şarkıcımız yine kendisi gibi şarkıcı olan sevgilisinden ayrıldıktan sonra tam evinin karşısındaki apartmanın tamamını kendi posteriyle kaplatmıştır?",
     options: ["Hande Yener", "Hadise", "Gülşen", "Simge"],
     answer: "Hadise",
-    media: "assets/pictures/billboard.jpg",
+    media: billboard,
   },
   {
     question: "Antik Roma'da kadınlar parfüm olarak ne kullanıyordu?",
     options: ["Gül Suyu", "Bal", "Gladyatör Teri", "Kan"],
     answer: "Gladyatör Teri",
-    media: "assets/pictures/parfum.jpg",
+    media: parfum,
   },
   {
     question: "T-Rex'in yaşayan en yakın akrabası aşağıdakilerden hangisidir?",
     options: ["İnekler", "Tavuklar", "Timsahlar", "Köpekler"],
     answer: "Tavuklar",
-    media: "assets/pictures/trex.jpg",
+    media: trex,
   },
   {
     question:
       "Her şeyin olduğu gibi mutluluğun da fobisi varmış. Bu fobiye ne ad verilir?",
     options: ["Çerofobi", "Euphobia", "Felicifobia", "Mutluluk Korkusu"],
     answer: "Çerofobi",
-    media: "assets/pictures/fobi.jpg",
+    media: fobi,
   },
 ];
 export default exams;
